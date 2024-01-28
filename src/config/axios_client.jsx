962abb407bff9e6b5e1c32d54a240e15a6edc334
nasridinov <nasridinrovfurkatjon@MacBook-Pro.bbrouter>
2024-01-28 12:33:50 +0500

    standart token

--- src/config/axios_client.jsx
+++ src/config/axios_client.jsx
@@ -4,7 +4,9 @@ import AppConstants from "./app_constants";
 const axiosClient = axios.create({
   baseURL: AppConstants.baseUrl,
   headers: {
-    Authorization: `Bearer ${localStorage.getItem(AppConstants.keyToken)}`,
+    //Authorization: `Bearer ${localStorage.getItem(AppConstants.keyToken)}`,
+    Authorization:
+      "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrNzk5OTIxNzI0OTQiLCJleHAiOjE3MDc4NDUwNDZ9.1U7hhJ3L4xFTFl6vtG1xQw1CjW2WTzb6yzaI4AU2eg-y60x_pIcnoolhStiBVyfNKp1BImWV4EHuea99WqO5oA",
     "Content-Type": "application/json",
   },
 });
