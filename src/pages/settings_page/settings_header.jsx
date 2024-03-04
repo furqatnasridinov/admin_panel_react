24b0db65e52254248e3f7d704f6037eae0fc93ee
nasridinov17 <nasridinov.f@karimov.group>
2024-03-04 21:05:39 +0500

    some changes

--- src/pages/settings_page/settings_header.jsx
+++ src/pages/settings_page/settings_header.jsx
@@ -4,8 +4,8 @@ import AppConstants from "../../config/app_constants";
 export default function SettingsHeader() {
   return (
     <div className="pl-[35px] pr-[19px] py-[32px] bg-white flex flex-col gap-1 rounded-[16px] ">
-      <div className="text-[14px] font-medium leading-[16px]" 
-      onClick={()=>{localStorage.removeItem(AppConstants.keyToken)}}>
+      <div className="text-[14px] font-medium leading-[16px] cursor-pointer"
+        onClick={() => { localStorage.removeItem(AppConstants.keyToken) }}>
         {/* {localStorage.getItem(AppConstants.keyToken)} */}
         Настойки
       </div>
