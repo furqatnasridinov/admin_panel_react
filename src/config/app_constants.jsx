fe00f3016edbd81213684309ca58d523a152827a
nasridinov17 <nasridinov.f@karimov.group>
2024-07-01 14:41:47 +0500

    firebase push

--- src/config/app_constants.jsx
+++ src/config/app_constants.jsx
@@ -54,6 +54,10 @@ class AppConstants {
     return "roleId";
   }
 
+  static get vapidKey() {
+    return "BKtMN6wjJ9LaMI3lhESMVyzTEMxTwC45q1hjAxgKijOf-e2uEYAmXSZrHirgj4lzx6XIkfxfybYDUgxdQWdTi9g";
+  }
+
   // yandex geocoding api key
   static get yandexApiKey() {
     return "55bfdcdc-b638-4a5e-81f0-404dc3e9060e";
