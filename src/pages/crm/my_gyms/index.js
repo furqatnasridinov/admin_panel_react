a0e8f92fc9e9f729ddb7152054926bec59e3e162
nasridinov17 <nasridinov.f@karimov.group>
2024-07-17 19:25:57 +0500

    doc viewer example

--- src/pages/crm/my_gyms/index.js
+++ src/pages/crm/my_gyms/index.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import DocViewer from "react-doc-viewer";
 
 export default function MyGymsPageCrm() {
-  return (
-    <div className='flex justify-center mt-[300px] text-[30px] text-crm-link'>MyGymsPageCrm</div>
-  );
+  const [doc, setDoc] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    fetch("https://myfit-russia.ru/doc/test.docx")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch document');
+        }
+        return [{ uri: "https://myfit-russia.ru/doc/test.docx" }];
+      })
+      .then(doc => setDoc(doc))
+      .catch(error => setError(error.message));
+  }, []);
+
+  if (error) {
+    return <div>Error loading document: {error}</div>;
+  }
+
+  return <DocViewer documents={doc} />;
 }
